420ed175534bcef2219e15be9f07fb6f8f3c01c8
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-11-01 15:50:57 -0600

    ASDFQASD

--- src/Store/Actions/index.js
+++ src/Store/Actions/index.js
@@ -10,7 +10,7 @@ export const ACTIONS = {
 export const collectNotes = () => dispatch => {
     console.log('Collect Notes');
     dispatch({type: ACTIONS.COLLECTING_NOTES});
-    axios.get('http://localhost:7777/api/notes/')
+    axios.get('https://brocknotes.herokuapp.com/api/notes')
     .then(response => (dispatch({type: ACTIONS.NOTES_COLLECTED, notes: response.data})))
     .catch(err => console.log('Unable to Collect Notes: ' + err.message))
 }
@@ -18,7 +18,7 @@ export const collectNotes = () => dispatch => {
 export const assembleNote = (note) => dispatch => {
     console.log('Assemble Note');
     dispatch({type: ACTIONS.ASSEMBLING_NOTE});
-    axios.post('http://localhost:7777/api/notes/', {...note})
+    axios.post('https://brocknotes.herokuapp.com/api/notes', {...note})
     .then(response => (dispatch({type: ACTIONS.NOTE_ASSEMBLED, note: {_id: response.data, ...note}})))
     .catch(err => console.log('Unable to Assemble Note: ' + err.message))
 }
@@ -26,7 +26,7 @@ export const assembleNote = (note) => dispatch => {
 export const reviseNote = (noteId, note) => dispatch => {
     console.log('Revise Note');
     dispatch({type: ACTIONS.REVISING_NOTE});
-    axios.put(`http://localhost:7777/api/notes/${noteId}`, note)
+    axios.put(`https://brocknotes.herokuapp.com/api/notes/${noteId}`, note)
     .then(response => (dispatch({type: ACTIONS.NOTE_REVISED, note: response.data, noteId: noteId})))
     .catch(err => console.log('Unable to Revise Note: ' + err.message))
 }
@@ -34,7 +34,7 @@ export const reviseNote = (noteId, note) => dispatch => {
 export const expungeNote = noteId => dispatch => {
     console.log('Expunge Note');
     dispatch({type: ACTIONS.EXPUNGING_NOTE});
-    axios.delete(`http://localhost:7777/api/notes/${noteId}`)
+    axios.delete(`https://brocknotes.herokuapp.com/api/notes/${noteId}`)
     .then(() => (dispatch({type: ACTIONS.NOTE_EXPUNGED, noteId: noteId})))
     .catch(err => console.log('Unable to Delete Note: ' + err.message))
 }

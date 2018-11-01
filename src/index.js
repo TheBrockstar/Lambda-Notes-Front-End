99019e422206094599df4ae3219e5a9b42540056
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-31 20:25:55 -0600

    Fixed Redux Devtools Issues
    Users without Redux Devtools were unable to access app. Fixed using a
    ternary operate that defaults to an empty compose() function.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './Components/App';
 import { reducer } from './Store/Reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const reduxDevToolsHook = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const reduxDevToolsHook = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose();
 
 const collection = createStore(reducer, compose(applyMiddleware(thunk, logger), reduxDevToolsHook));
 
